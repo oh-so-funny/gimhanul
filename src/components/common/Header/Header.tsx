f449d8c07b497daafd871d08e83d5c9827fef693
SEOKKAMONI <sj01048260279@gmail.com>
2023-07-05 18:39:40 +0900

    ADD :: login

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -1,11 +1,13 @@
 import font from "@/styles/font";
 import color from "@/styles/color";
 import styled from "styled-components";
+import Button from "../Button/Button";
 
 const Header = () => {
   return (
     <StyledHeader>
       <Logo>AI 면접 코칭 서비스</Logo>
+      <LoginButton>로그인</LoginButton>
     </StyledHeader>
   );
 };
@@ -25,6 +27,11 @@ const StyledHeader = styled.header`
 `;
 
 const Logo = styled.span`
+  ${font.H4}
+  color: ${color.white};
+`;
+
+const LoginButton = styled.button`
   ${font.H4}
   color: ${color.white};
 `;
